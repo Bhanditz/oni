8a710fab133e96f027cac83560aa3e4d1fc56867
Bryan Phelps <bryphe@outlook.com>
2017-11-03 14:07:14 -0700

    Fix issue with beforeEach/afterEach

--- test/common/index.ts
+++ test/common/index.ts
@@ -4,32 +4,35 @@ import * as assert from "assert"
 import { Oni } from "./Oni"
 
 export const runInProcTest = (testName: string, testPath: string, timeout: number = 5000) => {
-    let oni: Oni
 
-    beforeEach(async () => {
-        oni = new Oni()
-        return oni.start()
-    })
+    describe(testName, () => {
+        let oni: Oni
 
-    afterEach(async () => {
-        return oni.close()
-    })
+        beforeEach(async () => {
+            oni = new Oni()
+            return oni.start()
+        })
 
-    it("test: " + testName, async () => {
-        await oni.client.waitForExist(".editor", timeout)
-        const text = await oni.client.getText(".editor")
-        assert(text && text.length > 0, "Validate editor element is present")
+        afterEach(async () => {
+            return oni.close()
+        })
 
-        console.log("Test path: " + testPath) // tslint:disable-line
+        it("test: " + testName + "|" + testPath, async () => {
+            await oni.client.waitForExist(".editor", timeout)
+            const text = await oni.client.getText(".editor")
+            assert(text && text.length > 0, "Validate editor element is present")
 
-        oni.client.execute("Oni.automation.runTest('" + testPath + "')")
+            console.log("Test path: " + testPath) // tslint:disable-line
 
-        console.log("Waiting for result...") // tslint:disable-line
-        await oni.client.waitForExist(".automated-test-result", 30000)
-        const resultText = await oni.client.getText(".automated-test-result")
-        console.log("Got result: " + resultText) // tslint:disable-line
+            oni.client.execute("Oni.automation.runTest('" + testPath + "')")
 
-        const result = JSON.parse(resultText)
-        assert.ok(result.passed)
+            console.log("Waiting for result...") // tslint:disable-line
+            await oni.client.waitForExist(".automated-test-result", 30000)
+            const resultText = await oni.client.getText(".automated-test-result")
+            console.log("Got result: " + resultText) // tslint:disable-line
+
+            const result = JSON.parse(resultText)
+            assert.ok(result.passed)
+        })
     })
 }
